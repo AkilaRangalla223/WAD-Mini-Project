547389e08f9811b9ddf8cad8ee61dacb0a3c3133
Kasuri Abhilashani <abhilash_im17002@stu.kln.ac.lk>
2020-07-30 18:35:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+ /*JavaScript Document*/
+
+
+ function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+ //swiper script//
+ // JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+ 
